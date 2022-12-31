c22e94b9f1abbae398ec8f6ebd4b09a8a4878b7b
hama <hmmshl@gmail.com>
2023-01-01 00:17:34 +0100

    config.assets.compile added

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module QuoteApi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.enabled = false
+    config.serve_static_assets = false
+    config.assets.js_compressor = :uglifier
+    config.assets.compile = false
+    config.assets.digest = true
   end
 end
